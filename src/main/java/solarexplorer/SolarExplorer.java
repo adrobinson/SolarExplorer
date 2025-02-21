0d90454e8d3b6055d6ef3c5922ed4f370742a251
Adam Robinson <adrobinsonuk25@gmail.com>
2025-02-21 14:30:18 +0000

    added the sun as a light source, lined all planets up in order from the sun

--- src/main/java/solarexplorer/SolarExplorer.java
+++ src/main/java/solarexplorer/SolarExplorer.java
@@ -2,17 +2,20 @@ package solarexplorer;
 
 import com.jme3.app.SimpleApplication;
 import com.jme3.light.DirectionalLight;
+import com.jme3.light.PointLight;
 import com.jme3.material.Material;
 import com.jme3.math.ColorRGBA;
 import com.jme3.math.FastMath;
 import com.jme3.math.Quaternion;
 import com.jme3.math.Vector3f;
+import com.jme3.post.FilterPostProcessor;
 import com.jme3.renderer.RenderManager;
 import com.jme3.scene.Geometry;
-import com.jme3.scene.shape.Box;
+import com.jme3.scene.Spatial;
 import com.jme3.scene.shape.Sphere;
 import com.jme3.system.AppSettings;
 import com.jme3.texture.Texture;
+import com.jme3.util.SkyFactory;
 
 /**
  * This is the Main Class of your Game. It should boot up your game and do initial initialisation
@@ -50,56 +53,89 @@ public class SolarExplorer extends SimpleApplication {
 
     }
 
+    private void prepareSun(){
+
+        // Calculate size of the Sun compared to Earth
+        // (432,690 / 3959) * 100 = 10929%
+        // Earth radius is size of '1' so sun's radius is '109.29'
+        Sphere sunMesh = new Sphere(32, 32, (float) 109.29);
+        sunMesh.setTextureMode(Sphere.TextureMode.Projected);
+
+        Geometry sun = new Geometry("Sphere", sunMesh);
+
+        Material material = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md"); // Unshaded so object does not need external light source to be visible
+        Texture texture = assetManager.loadTexture("Textures/sun_dmap.jpg");
+         material.setTexture("ColorMap", texture);
+        sun.setMaterial(material);
+        sun.setLocalTranslation(0, 0,0);
+        rootNode.attachChild(sun);
+
+        PointLight sunlight = new PointLight();
+        sunlight.setColor(ColorRGBA.White.mult(10));
+        sunlight.setRadius(10000f);
+        sunlight.setPosition(sun.getLocalTranslation());
+        rootNode.addLight(sunlight);
+
+    }
+
+    private void setBackground(){
+        Texture spaceTexture = assetManager.loadTexture("Textures/space_background.jpg");
+        Spatial sky = SkyFactory.createSky(assetManager, spaceTexture, SkyFactory.EnvMapType.EquirectMap);
+        rootNode.attachChild(sky);
+    }
+
     @Override
     public void simpleInitApp() {
 
+        cam.setLocation(new Vector3f(150, 0, 150));
         flyCam.setMoveSpeed(50);
+        setBackground();
 
         // All planet sizes will be calculated in reference to the Earth
         // Earth Radius = 3959 (miles)
         Geometry earth = preparePlanet(1f, "Textures/earth_dmap.jpg");
+        earth.setLocalTranslation(160, -20, 0);
         rootNode.attachChild(earth);
 
         // Mercury Radius = 2106 (miles)
         // Mercury's Radius calculated as a percentage of Earth's radius:
         // (1516 / 3959) * 100 = 38% (0.38/1)
         Geometry mercury = preparePlanet(0.38f, "Textures/mercury_dmap.jpg");
-        mercury.setLocalTranslation(-5, 0, 0); // I have set locations temporarily just so you can see all the planets
+        mercury.setLocalTranslation(120, -20, 0); // I have set locations temporarily just so you can see all the planets
         rootNode.attachChild(mercury);
 
         // (3760 / 3959) * 100 = 94%
         Geometry venus = preparePlanet(0.94f, "Textures/venus_dmap.jpg");
-        venus.setLocalTranslation(-3, 0, 0);
+        venus.setLocalTranslation(140, -20, 0);
         rootNode.attachChild(venus);
 
         // (2106 / 3959) * 100 = 53%
         Geometry mars = preparePlanet(0.53f, "Textures/mars_dmap.jpg");
-        mars.setLocalTranslation(3, 0, 0);
+        mars.setLocalTranslation(180, -20, 0);
         rootNode.attachChild(mars);
 
         // (43,441 / 3959) * 100 = 1097%
         Geometry jupiter = preparePlanet(10.97f, "Textures/jupiter_dmap.jpg");
-        jupiter.setLocalTranslation(0, 0, -30);
+        jupiter.setLocalTranslation(210, -20, 0);
         rootNode.attachChild(jupiter);
 
         // (36,184 / 3959) * 100 = 913%
         Geometry saturn = preparePlanet(9.13f, "Textures/jupiter_dmap.jpg");
-        saturn.setLocalTranslation(20, 0, -20);
+        saturn.setLocalTranslation(250, -20, 0);
         rootNode.attachChild(saturn);
 
         // (15,759 / 3959) * 100 = 398%
         Geometry uranus = preparePlanet(3.98f, "Textures/uranus_dmap.jpg");
-        uranus.setLocalTranslation(7, 0, -5);
+        uranus.setLocalTranslation(290, -20, 0);
         rootNode.attachChild(uranus);
 
-        // (15,299 / 3959) * 100 = 386%11
+        // (15,299 / 3959) * 100 = 386%
         Geometry neptune = preparePlanet(3.86f, "Textures/neptune_dmap.jpg");
-        neptune.setLocalTranslation(15, 0, -5);
+        neptune.setLocalTranslation(320, -20, 0);
         rootNode.attachChild(neptune);
 
-        DirectionalLight sun = new DirectionalLight(); // Adds light so objects can be seen
-        sun.setDirection(new Vector3f(10f, -10f, -1f).normalizeLocal());
-        rootNode.addLight(sun);
+        prepareSun();
+
     }
 
     @Override
